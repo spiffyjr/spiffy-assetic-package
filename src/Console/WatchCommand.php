e485aa2a874d96b819533cedae6f069d047f0d71
Kyle Spraggs <theman@spiffyjr.me>
2014-07-12 00:21:17 -0500

    Fix watch command with import

--- src/Console/WatchCommand.php
+++ src/Console/WatchCommand.php
@@ -7,6 +7,7 @@ use Spiffy\Inject\InjectorAware;
 use Spiffy\Inject\InjectorAwareTrait;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class WatchCommand extends Command implements InjectorAware
